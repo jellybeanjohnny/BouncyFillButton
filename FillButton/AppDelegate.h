472671c6d0aef07e36fa2f91ee3c0258b6e1822b
Matt Amerige <matt@wubbyland.com>
2016-01-06 22:21:28 -0500

    Initial Commit

---
+++ FillButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FillButton
+//
+//  Created by Matt Amerige on 1/6/16.
+//  Copyright © 2016 Wubbyland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
